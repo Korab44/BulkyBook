f57801662d6256f0520828c1433f9f2d76c8f5d9
korab <korab.morina444@gmail.com>
2023-10-13 16:24:18 +0200

    Create Category Controller, Retrieve all Categories, Display all Categories, Bootswatch Theme, Bootstrap Icons, Create Category View, Demo- Create Category, Server Side Validation, Custom Validation, Add Client Side Validation and Display Name and Range Validation

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,43 @@
+﻿using BulkyBookWeb.data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _context.Categories;
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }   
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if(category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Displayorder can not exactly match the Name.");
+            }
+            if (!ModelState.IsValid)
+            {
+                return (View(category));
+            }
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
